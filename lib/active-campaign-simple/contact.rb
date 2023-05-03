45c4bce2ad879a29e791f3521d2adb0bcea2bdb8
Charles Max Wood <cmaxw@devchat.tv>
2023-05-03 09:47:37 -0600

    Add tag to contact

--- lib/active-campaign-simple/contact.rb
+++ lib/active-campaign-simple/contact.rb
@@ -2,12 +2,23 @@ require 'active-campaign-simple/config'
 
 module ActiveCampaign
   class Contact
+    attr_reader :attributes
+
     def initialize(attributes)
       @attributes = attributes
     end
 
+    def id
+      @attributes['id']
+    end
+
+    def add_tag(tag)
+      tag_to_add = Tag.find(tag)
+      ActiveCampaign.post('/contactTags', { contactTag: { contact: id, tag: tag_to_add.id } })
+    end
+
     def self.create_or_find_by_email(email)
-      new(search(email).first || create({ email: email }))
+      search(email).first || new(create({ email: email }))
     end
 
     def self.create(payload)
@@ -15,7 +26,9 @@ module ActiveCampaign
     end
 
     def self.search(term)
-      ActiveCampaign.get('/contacts', query: { search: term })['contacts']
+      ActiveCampaign.get('/contacts', query: { search: term })['contacts'].map do |attributes|
+        new(attributes)
+      end
     end
   end
 end
