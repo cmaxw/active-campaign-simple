60f774eede4cdeb45d5037c82d4f4d00ffa27f80
Charles Max Wood <cmaxw@devchat.tv>
2023-05-02 21:45:31 -0600

    Basic email Contact API

--- lib/active-campaign-simple.rb
+++ lib/active-campaign-simple.rb
@@ -2,19 +2,22 @@ require 'active-campaign-simple/client'
 require 'active-campaign-simple/config'
 require 'active-campaign-simple/logger'
 
+require 'active-campaign-simple/contact'
+
 module ActiveCampaign
   extend Config
   class << self
     # Alias for ActiveCampaign::Client.new
     #
     # @return [ActiveCampaign::Client]
-    def new(options={})
+    def new(options = {})
       ActiveCampaign::Client.new(options)
     end
 
     # Delegate to ActiveCampaign::Client
     def method_missing(method, *args, **kwargs)
       return super unless new.respond_to?(method)
+
       new.send(method, *args, **kwargs)
     end
 
--- lib/active-campaign-simple/contact.rb
+++ lib/active-campaign-simple/contact.rb
@@ -2,8 +2,20 @@ require 'active-campaign-simple/config'
 
 module ActiveCampaign
   class Contact
+    def initialize(attributes)
+      @attributes = attributes
+    end
+
+    def self.create_or_find_by_email(email)
+      new(search(email).first || create({ email: email }))
+    end
+
     def self.create(payload)
-      ActiveCampaign::Client.post('/contacts', payload: payload)
+      new(ActiveCampaign.post('/contacts', payload: { contact: payload }))
+    end
+
+    def self.search(term)
+      ActiveCampaign.get('/contacts', search: term)
     end
   end
 end
